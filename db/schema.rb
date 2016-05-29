8c26410a9153130595c516344ab87234dec21662
Emily Dowdle <emily@editingemily.com>
2016-05-29 10:42:38 -0600

    Set up asset pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20160528163639) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "incidents", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
