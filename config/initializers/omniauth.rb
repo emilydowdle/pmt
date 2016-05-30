8707c72b07f2ff355c33041ebb4f5d43c3f1348b
Emily Dowdle <emily@editingemily.com>
2016-05-30 10:17:38 -0600

    Add scope to Oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,19 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.full_host = Rails.env.production? ? 'https://blk-box.herokuapp.com' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  {
+    :name => "google",
+    :scope => "email, profile, plus.me, http://gdata.youtube.com",
+    :prompt => "select_account",
+    :image_aspect_ratio => "square",
+    :image_size => 50
+  },
+  {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
+
+
+# https://www.googleapis.com/auth/plus.login	Know the list of people in your circles, your age range, and language
+# https://www.googleapis.com/auth/plus.me	Know who you are on Google
+# https://www.googleapis.com/auth/userinfo.email	View your email address
+# https://www.googleapis.com/auth/userinfo.profile	View your basic profile info
