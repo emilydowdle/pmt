b9f4c292bb5c2c71702e43e4ca42d591dccad709
Emily Dowdle <emily@editingemily.com>
2016-05-30 09:32:02 -0600

    Update env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
+OmniAuth.config.full_host = Rails.env.production? ? 'https://blk-box.herokuapp.com' : 'http://localhost:3000'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
