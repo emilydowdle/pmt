cd23f926d3159beaa747b62cecf7298c8f103e4a
Emily Dowdle <emily@editingemily.com>
2016-06-07 20:31:54 -0600

    Closes #27, bitches

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,9 @@
 class Organization < ActiveRecord::Base
   extend AttrEncrypted
+  validates :name, presence: true,
+                   uniqueness: true
+  validates :pagerduty_account, presence: true
+  validates :pagerduty_token, presence: true
 
   attr_encrypted :pagerduty_account, key: ENV['ENCRYPTION_KEY'], encode: true
   attr_encrypted :pagerduty_token, key: ENV['ENCRYPTION_KEY'], encode: true
@@ -7,5 +11,4 @@ class Organization < ActiveRecord::Base
   has_many :organization_users
   has_many :users, through: :organization_users
   has_many :blackboxes
-  validates :name, uniqueness: true
 end
